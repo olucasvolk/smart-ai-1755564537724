9d82457078f7485f9cc211e427e704524e2f6949
Lucas William <107009649+olucasvolk@users.noreply.github.com>
2025-08-18 21:49:05 -0300

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Layout from '../layout';
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <div className='p-4'>
+        <h1 className='text-2xl font-bold mb-4'>Sales Dashboard</h1>
+        {/* Add charts here */}
+      </div>
+    </Layout>
+  );
+}
